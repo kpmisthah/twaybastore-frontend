b7ffa866bb7ad64dea4f141b817327b97da707ac
kpmisthah <kpmisthah6@gmail.com>
2025-12-27 20:33:05 +0530

    fix: add trailing slash to BASE_URL in frontend config

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,2 @@
-// const BASE_URL = "https://twayba-backend-oln6.onrender.com/api/";
-const BASE_URL = "https://api.twayba.com/api";
+const BASE_URL = "https://api.twayba.com/api/";
 export default BASE_URL;
